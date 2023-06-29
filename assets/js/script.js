d7c6fb1e135a82deb8f3e9ffd4e23a85bc6f534b
Brian Gorman <briangorman99@gmail.com>
2023-06-29 17:02:51 -0400

    rename letter to letters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Global variables
-var letter = "abcdefghijklmnopqrstuvwxyz".split("");
+var letters = "abcdefghijklmnopqrstuvwxyz".split("");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChars = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 
