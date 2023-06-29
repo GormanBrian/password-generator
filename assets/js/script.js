b102a4a65eb2e61a41ff1b2492538a66c3cfa202
Brian Gorman <briangorman99@gmail.com>
2023-06-29 16:21:15 -0400

    fix html assets links, fix confirm dialog if statements, add length requirement prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var count = 20;
 const uppercase = [];
 const lowercase = [];
+const numbers = [];
 const specialChars = [];
 
 // Get reference to the element with id 'generate'
@@ -9,13 +10,25 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   let options = [];
-  if (confirm("Should the password include uppercase letters?"))
+  if (confirm("Should the password include uppercase letters?")) {
     options.concat(uppercase);
-  if (confirm("Should the password include lowercase letters?"))
+  }
+  if (confirm("Should the password include lowercase letters?")) {
     options.concat(lowercase);
-  if (confirm("Should the password include numbers?")) options.concat(numbers);
-  if (confirm("Should the password include special characters?"))
+  }
+  if (confirm("Should the password include numbers?")) {
+    options.concat(numbers);
+  }
+  if (confirm("Should the password include special characters?")) {
     options.concat(specialChars);
+  }
+
+  let count = parseInt(prompt("Enter the length of the password"));
+  while (count < 8 || count > 128) {
+    count = prompt(
+      "That number is not between 8 and 128. Please enter a valid password length."
+    );
+  }
 }
 
 // Write password to the #password input
